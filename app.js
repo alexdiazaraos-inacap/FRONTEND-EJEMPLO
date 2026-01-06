9e40ef0d819f1e52a7fd0ba3c3ab827215229b6f
Alex Diaz Araos <alex.diaz12@inacapmail.cl>
2026-01-05 21:45:50 -0300

    feature: Validación y manejo en app.js

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 document.getElementById('btn').addEventListener('click', () => {
   alert('¡Funciona!');
 });
+
+const inputDireccion = document.getElementById('direccion');
+document.getElementById('direccion').addEventListener('blur', () => {
+  const valor = inputDireccion.value.trim();
+  if (!valor) {
+    alert('Por favor, ingresa tu dirección.');
+    return;
+  }
+  alert('Dirección guardada: ${valor}');
+});
